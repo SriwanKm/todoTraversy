3b09f392e48a9f8c38eca8eadeaac9b9de1d78bb
KCQK <kcqkoo@gmail.com>
2021-01-29 15:38:16 -0500

    initial commit

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+O
+src/components/layout/Header.js,0\5\0544fa720c16d63262bd906377f2d06a1834c9d0
---
+++ src/index.js
@@ -0,0 +1,9 @@
+// Everything in the project end up running through this file
+
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+// Everything out put here (root)
+ReactDOM.render(<App />, document.getElementById('root'));
